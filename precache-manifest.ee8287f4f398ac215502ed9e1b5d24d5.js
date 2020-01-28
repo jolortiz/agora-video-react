762c6d7c224bbb238436bbf7ab1549500b2b436a
jolortiz <jolortiz@ucsc.edu>
2020-01-27 17:33:28 -0800

    Updates

---
+++ precache-manifest.ee8287f4f398ac215502ed9e1b5d24d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1597d7e3cb5e91dd34f776672d2a25b",
+    "url": "/agora-video-react/index.html"
+  },
+  {
+    "revision": "3002b4aae3e218e39d95",
+    "url": "/agora-video-react/static/css/2.bd936ec9.chunk.css"
+  },
+  {
+    "revision": "976e12d4ea9c9e3b4ba0",
+    "url": "/agora-video-react/static/css/main.ff19311d.chunk.css"
+  },
+  {
+    "revision": "3002b4aae3e218e39d95",
+    "url": "/agora-video-react/static/js/2.7c3d3ba7.chunk.js"
+  },
+  {
+    "revision": "976e12d4ea9c9e3b4ba0",
+    "url": "/agora-video-react/static/js/main.8f489a2a.chunk.js"
+  },
+  {
+    "revision": "db0c5c13ccde56b071d7",
+    "url": "/agora-video-react/static/js/runtime~main.7d1575ad.js"
+  }
+]);
